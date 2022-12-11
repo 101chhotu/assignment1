bda5aa02cdba274f7a2e9b8a159ac517c8117ba3
Ravaliya_khushi <110173132+101chhotu@users.noreply.github.com>
2022-12-11 20:33:32 +0530

    Update Q_6.php

--- Q_6.php
+++ Q_6.php
@@ -1,21 +1,22 @@
 <html>
-<head>
-<body>
-<div class="container">
-<form method="POST">
-<div>
-<lable>enter number:</lable>
-</div>
-<div class="row">
-<div class="col-6">
-<input type="text" name="num" placeholder="enter number here" autocomplete="off">
-</div>
-<div class="col-6">
-    <input type="submit" name="btn" value="submit">
-</div>
-</div>
-</form>
-</div>
+	<body>
+		<div class="container">
+			<form method="POST">
+				<div>
+				<lable>enter number:</lable>
+				</div>
+				<div class="row">
+					<div class="col-6">
+					<input type="text" name="num" placeholder="enter number here" autocomplete="off">
+					</div>
+					<div class="col-6">
+					    <input type="submit" name="btn" value="submit">
+					</div>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
 <?php
 if(isset($_POST['num']))
 {
@@ -36,6 +37,3 @@ else{
 	echo "no number has been entered !!!";
 }	
 ?>
-</body>
-</head>
-</html>
