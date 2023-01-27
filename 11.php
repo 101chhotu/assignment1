01c1043bb3e188c178ab7a9e7b69fdcb35550a0e
Ravaliya_khushi <110173132+101chhotu@users.noreply.github.com>
2023-01-27 09:13:54 +0530

    Add files via upload

---
+++ 11.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+         <link rel="stylesheet" href="bootstrap.min.css"/>
+    </head>
+    <style>
+        body{
+            background:linear-gradient(white,pink);
+        }
+    </style>
+      <body>
+    
+<ul class="nav bg-dark text-white">
+                <li><a href="1.php" class="btn p-3 text-white font-weight-italic">Q1</a></li>
+                <li><a href="2.php" class="btn p-3 text-white font-weight-italic">Q2</a></li>
+                <li><a href="3.php" class="btn p-3 text-white font-weight-italic">Q3</a></li>
+                <li><a href="4.php" class="btn p-3 text-white font-weight-italic">Q4</a></li>
+                <li><a href="5.php" class="btn p-3 text-white font-weight-italic">Q5</a></li>
+                <li><a href="6.php" class="btn p-3 text-white font-weight-italic">Q6</a></li>
+                <li><a href="7.php" class="btn p-3 text-white font-weight-italic">Q7</a></li>
+                <li><a href="8.php" class="btn p-3 text-white font-weight-italic">Q8</a></li>
+                <li><a href="9.php" class="btn p-3 text-white font-weight-italic">Q9</a></li>
+                <li><a href="10.php" class="btn p-3 text-white font-weight-italic">Q10</a></li>
+                <li><a href="11.php" class="btn p-3 text-white font-weight-italic">Q11</a></li>
+                <li><a href="12.php" class="btn p-3 text-white font-weight-italic">Q12</a></li>
+            </ul>
+            <div class="bg-white text-dark p-4 fixed-bottom text-center"><strong>PHP ASSIGNMENT-1 BY RAVALIYA__</strong></div>
+  
+    <body>
+    &nbsp;  &nbsp; &nbsp; &nbsp;A<br>
+    &nbsp; &nbsp; &nbsp;BAB&nbsp;<br>
+    &nbsp;  &nbsp;CBABC<br>
+    &nbsp;  DCBABCD<br>
+        EDCBABCDE
+
+    </body>
+</html>
